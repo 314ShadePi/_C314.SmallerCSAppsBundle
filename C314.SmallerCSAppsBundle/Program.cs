0143cf207e726798526315e574b388423cba976b
314ShadePi <erikkirkegaardstage@gmail.com>
2022-02-27 12:22:01 +0100

    Add project files.

---
+++ C314.SmallerCSAppsBundle/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using C314.SmallerCSAppsBundle.GetMCServer;
+
+#region Setup
+Console.Title = "314 Smaller C# Programs Bundle";
+#endregion
+
+Console.WriteLine("Hello, World!");
+// System.Management.Automation Broken
+GetMCServer.GetMCServerMain();
+Console.ReadKey();
