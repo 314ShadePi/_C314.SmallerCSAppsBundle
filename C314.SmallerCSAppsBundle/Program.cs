f87d2c2c393547c86f8d6f517783f5c571984dd1
314ShadePi <erikkirkegaardstage@gmail.com>
2022-03-01 16:22:31 +0100

    Add "ProjOne" to solution

--- C314.SmallerCSAppsBundle/Program.cs
+++ C314.SmallerCSAppsBundle/Program.cs
@@ -1,11 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using C314.SmallerCSAppsBundle.GetMCServer;
+// using C314.SmallerCSAppsBundle.GetMCServer;
+// System.Management.Automation Broken
+// GetMCServer.GetMCServerMain();
+
+#region Usings
+using C314.SmallerCSAppsBundle.ProjOne;
+#endregion
 
 #region Setup
-Console.Title = "314 Smaller C# Programs Bundle";
+const string title = "314 Smaller C# Programs Bundle";
+
+static void Setup()
+{
+    Console.Title = title;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.TreatControlCAsInput = true;
+}
+Setup();
 #endregion
 
 Console.WriteLine("Hello, World!");
-// System.Management.Automation Broken
-GetMCServer.GetMCServerMain();
+Console.ReadKey();
+KeyCodes.KeyCodesMain();
+Setup();
+Console.WriteLine("Hello, World!");
 Console.ReadKey();
---
+++ ProjOne/KeyCodes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C314.SmallerCSAppsBundle.ProjOne
+{
+    public class KeyCodes
+    {
+        public static void KeyCodesMain()
+        {
+            Console.TreatControlCAsInput = true;
+            Console.Title = "Input Viewer";
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            ConsoleKeyInfo consoleKeyInfo;
+
+            do
+            {
+                consoleKeyInfo = Console.ReadKey();
+                Console.Write(" -> ");
+                if ((consoleKeyInfo.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
+                if ((consoleKeyInfo.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
+                if ((consoleKeyInfo.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+");
+                Console.WriteLine(consoleKeyInfo.Key.ToString());
+            } while (consoleKeyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
