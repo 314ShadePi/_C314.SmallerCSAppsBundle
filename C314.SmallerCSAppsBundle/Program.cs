7a6dc41c370d1ef0d6758441b055601e303fea77
Erik Kirkegaard Stage <erik6541@hillskole.dk>
2022-03-02 09:33:16 +0100

    Create Programchooser

--- C314.SmallerCSAppsBundle/Program.cs
+++ C314.SmallerCSAppsBundle/Program.cs
@@ -16,12 +16,27 @@ static void Setup()
     Console.ForegroundColor = ConsoleColor.White;
     Console.TreatControlCAsInput = true;
 }
+Start:
 Setup();
 #endregion
 
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
-KeyCodes.KeyCodesMain();
-Setup();
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+Console.WriteLine("Please choose what program you want to run:");
+Console.WriteLine("<Key To Press> -> <Project That Will Be Launched>");
+Console.WriteLine("Escape -> Quit");
+Console.WriteLine("1 -> Key Codes");
+ConsoleKeyInfo ChosenProject = Console.ReadKey();
+
+switch (ChosenProject.Key.ToString())
+{
+    case "D1":
+        Console.WriteLine("\nProgram chosen: \"Key Codes\", press any key to confirm or \"Escape\" to cancel");
+        ConsoleKeyInfo Confirm = Console.ReadKey();
+        Console.WriteLine();
+        if (Confirm.Key.ToString() == "Escape") goto Start;
+        KeyCodes.KeyCodesMain();
+        goto Start;
+    case "Escape":
+        return 0;
+    default:
+        goto Start;
+}
