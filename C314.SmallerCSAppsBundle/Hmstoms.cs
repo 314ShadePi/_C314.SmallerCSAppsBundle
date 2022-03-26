db83430d95df847a1ba75de50741c932c5df32d8
314ShadePi <erikkirkegaardstage@gmail.com>
2022-03-26 20:08:59 +0100

    7: Change command system to be commandline only

---
+++ C314.SmallerCSAppsBundle/Hmstoms.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace C314.SmallerCSAppsBundle
+{
+    [Verb("hmstoms", HelpText = "Converts Hours, Minutes and Seconds to milliseconds. Input should be seperated with spaces")]
+    public class Hmstoms
+    {
+        [Value(0, HelpText = "Hours")]
+        public int H { get; set; }
+        [Value(1, HelpText = "Minutes")]
+        public int M { get; set; }
+        [Value(2, HelpText = "Seconds")]
+        public int S { get; set; }
+    }
+}
--- C314.SmallerCSAppsBundle/Program.cs
+++ C314.SmallerCSAppsBundle/Program.cs
@@ -1,5 +1,5 @@
 ﻿using CommandLine;
-using C314.SmallerCSAppsBundle.Verbs;
+using System.Reflection;
 
 namespace C314.SmallerCSAppsBundle
 {
@@ -7,21 +7,51 @@ namespace C314.SmallerCSAppsBundle
     {
         static void Main(string[] args)
         {
-            Type[] types = { typeof(hmstoms) };
-            _ = Parser.Default.ParseArguments(args, types)
+            //Type[] types = { typeof(Options), typeof(Hmstoms) };
+            Type[] types = LoadVerbs();
+            Parser.Default.ParseArguments(args, types)
                   .WithParsed(Run)
-                  .WithNotParsed(errors => CErrorHandlers.HandleParseError(errors));
+                  .WithNotParsed(HandleErrors);
+        }
+
+        private static Type[] LoadVerbs()
+        {
+            return Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => t.GetCustomAttribute<VerbAttribute>() != null).ToArray();
+        }
+
+        private static void HandleErrors(IEnumerable<Error> errors)
+        {
+            if (errors.IsVersion())
+            {
+                Console.WriteLine("Version Request");
+                return;
+            }
+
+            if (errors.IsHelp())
+            {
+                Console.WriteLine("Help Request");
+                return;
+            }
+            Console.WriteLine("Parser Fail");
         }
 
         private static void Run(object obj)
         {
             switch (obj)
             {
-                case hmstoms h:
-                    SingleMethodCommands.Time.HMSToMs(h.Hms);
+                case Options o:
+                    if (o.Keycodes)
+                    {
+                        ProjOne.KeyCodes.KeyCodesMain();
+                        break;
+                    }
+                    break;
+                case Hmstoms o:
+                    Console.WriteLine(SingleMethodCommands.Time.HMSToMs(o.H, o.M, o.S));
                     break;
                 default:
-                    Console.WriteLine("Error");
+                    Console.WriteLine(obj.ToString());
                     break;
             }
         }
